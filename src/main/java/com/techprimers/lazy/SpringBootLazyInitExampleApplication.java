f0d5a04e3c5b90602d6a0eedd3deb9f4cab2201d
root <root@ubuntu-jenkins.us-central1-a.c.psyched-option-261004.internal>
2020-06-02 19:11:47 +0000

    one work

--- src/main/java/com/techprimers/lazy/SpringBootLazyInitExampleApplication.java
+++ src/main/java/com/techprimers/lazy/SpringBootLazyInitExampleApplication.java
@@ -2,7 +2,7 @@ package com.techprimers.lazy;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+// nothing
 @SpringBootApplication
 public class SpringBootLazyInitExampleApplication {
 
